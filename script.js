ec776af5dbfff7591727e840321c788efcd964ae
maoyubin <maoyubinabc@163.com>
2017-03-28 20:41:54 +0800

    add top button

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // An empty js file added to prevent animations firing on page loading
+jQuery(document).ready(function() {
+	var offset = 220;
+	var duration = 500;
+	jQuery(window).scroll(function() {
+		var browserWidth = jQuery(window).width();
+
+		//if(browserWidth > 450){
+			if (jQuery(this).scrollTop() > offset) {
+				jQuery('.back-to-top').fadeIn(duration);
+			} else {
+				jQuery('.back-to-top').fadeOut(duration);
+			}
+		//}
+	});
+
+	jQuery('.back-to-top').click(function(event) {
+		event.preventDefault();
+		jQuery('html, body').animate({scrollTop: 0}, duration);
+		return false;
+	})
+});
