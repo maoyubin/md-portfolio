a09777711103eb5e9cb605eed2e6a40f90be3ab1
maoyubin <maoyubinabc@163.com>
2017-03-29 10:46:22 +0800

    add

--- script.js
+++ script.js
@@ -18,5 +18,19 @@ jQuery(document).ready(function() {
 		event.preventDefault();
 		jQuery('html, body').animate({scrollTop: 0}, duration);
 		return false;
-	})
+	});
+		
+	$('a[href*="#port--"]').click(function() {
+	  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+	    var target = $(this.hash);
+	    console.log(target);
+	    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	    if (target.length) {
+	      $('html, body').animate({
+	        scrollTop: target.offset().top
+	      }, 1000);
+	      return false;
+	    }
+	  }
+	});
 });
